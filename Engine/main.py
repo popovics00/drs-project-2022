f0385ec2d803ca19082033d10777699b4cf41e2b
BozidarNesicPR80 <neskebroo@gmail.com>
2023-01-18 17:46:22 +0100

    Dockerize an app

--- Engine/main.py
+++ Engine/main.py
@@ -8,6 +8,9 @@ from blueprints.getTransactions import getTransactions_bp
 from flask_cors import CORS
 from db_config import *
 
+with app.app_context():
+    db.create_all()
+
 app.register_blueprint(auth)
 app.register_blueprint(user_stats)
 app.register_blueprint(deposit)
@@ -15,9 +18,9 @@ app.register_blueprint(getCrypto_bp)
 app.register_blueprint(transactionCrypto_bp)
 app.register_blueprint(getTransactions_bp)
 
-db.create_all(app=app)
+#db.create_all(app=app)
 CORS(app)
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
     
